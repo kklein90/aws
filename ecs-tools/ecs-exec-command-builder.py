1a94f6a064e01a851ac551e7c6796c89d20aafcb
kklein90 <kklein90@gmail.com>
2024-05-08 13:31:30 -0400

    ecs exec cmd script

--- ecs-tools/ecs-exec-command-builder.py
+++ ecs-tools/ecs-exec-command-builder.py
@@ -2,56 +2,84 @@ import boto3
 import subprocess
 
 try:
+    import argparse
+except ModuleNotFoundError as e:
+    print("this script requires the argparse python module")
+    exit()
+    
+try:
     import inquirer
 except ModuleNotFoundError as e:
     print("this script requires the inquirer python module")
     exit()
 
-client=boto3.client('ecs')
 
-## generate list of existing services
-# find cluster name
-cluster_arn = client.list_clusters(maxResults=1)
-cluster_name = cluster_arn['clusterArns'][0].split('/')[1]
+def get_env_client(account, region, svc):
+    session = boto3.Session (profile_name=account, region_name=region)
+    client = session.client(svc)
+    return client
 
-# list services on cluster
-svc_list = []
-services = client.list_services(cluster=cluster_name, maxResults=10)
-for svc in services['serviceArns']:
-    last = len(svc.split("/"))
-    svc_list.append(svc.split("/")[last - 1][:15])
+def prompt_user(cont_list):
+    cont_list = cont_list
+    # prompt user
+    questions = [
+        inquirer.List('service',
+                    message="Which container to connect to?",
+                    choices = cont_list
+                    ),
+    ]
 
-# prompt user
-questions = [
-    inquirer.List('service',
-                  message="Which service to connect to?",
-                  choices = svc_list
-                  ),
-]
+    svc_conn = inquirer.prompt(questions)
 
-svc_conn = inquirer.prompt(questions)
+    return svc_conn
 
-# find taskId
-## need family names to find task id services
-families = client.list_task_definition_families(familyPrefix=svc_conn['service'])
+def get_container_list(account, region):
+    client = get_env_client(account, region, 'ecs')
+    cluster_name = client.list_clusters(maxResults=1)['clusterArns'][0].split('/')[1]
 
-# won't work for tasks with only 1 container
-for family in families['families']:
-    tasks = client.list_tasks(cluster=cluster_name, family=family) # retuns taskArns
-    task_id = tasks['taskArns'][0].split('/')[2]
+    task_info={}
+    cont_list=[]
+    task_arns = client.list_tasks(cluster=cluster_name)
+    for task_arn in task_arns['taskArns']:
+        t_id = task_arn.split('/')[2]
+        c_name = client.describe_tasks(cluster=cluster_name, tasks=[t_id])['tasks'][0]['containers'][0]['name']
+        task_info[c_name] = t_id
+        cont_list.append(c_name)
+    # print(task_info)
+    # print(cont_list)
 
-# find container name
-task_details = client.describe_tasks(cluster=cluster_name, tasks=[task_id])
+    return cont_list, task_info, cluster_name
 
-for task in task_details['tasks'][0]['containers']:
-    if task['name'] == 'datadog-agent':  # don't want DD container
-        continue
-    else:
-        cont_name = task['name']
+def exec_cont(cont_name, task_id, cluster, region):
+    print("Connecting to: ")
+    print("cluster name: " + cluster_name)
+    print("task ID: " + task_id)
+    print("container name: " + cont_name)
+    subprocess.run(["aws", "--region", region, "--profile", account, "ecs", "execute-command", "--cluster", cluster_name, "--task", task_id, "--container", cont_name, "--interact", "--command", "/bin/sh"])
 
-print("Connecting to: ")
-print("cluster name: " + cluster_name)
-print("task ID: " + task_id)
-print("container name: " + cont_name)
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='Login to AWS')
+    parser.add_argument(
+        "--account", "-a",
+        type=str,
+        default="nonprod",
+        help="AWS account to access (nonprod, production) (default: %(default)s)"
+    )
+    parser.add_argument(
+        "--region", "-r",
+        type=str,
+        default="us-east-1",
+        help="AWS region to access (us-east-1,ca-west-1,eu-west-1) (default: %(default)s)"
+    )
+    pargs = parser.parse_args()
+    account = pargs.account
+    region = pargs.region
 
-subprocess.run(["aws", "ecs", "execute-command", "--cluster", cluster_name, "--task", task_id, "--container", cont_name, "--interact", "--command", "/bin/sh"])
+    cont_list, task_info, cluster_name = get_container_list(account, region)
+    cont_name = prompt_user(cont_list)['service']
+    task_id = task_info[cont_name]
+    exec_cont(cont_name, task_id, cluster_name, region)
+
+
+
+
